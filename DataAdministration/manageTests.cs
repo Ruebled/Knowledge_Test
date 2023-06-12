f1b6a3307f3a225384fe55dde4410797349c6adb
Ruebled <dan.canevschii@gmail.com>
2023-06-12 21:31:11 +0300

    Added Retrieve Tests from file

--- DataAdministration/manageTests.cs
+++ DataAdministration/manageTests.cs
@@ -10,17 +10,19 @@ namespace DataAdministration
     public class ManageTests
     {
         private string TESTS_DATA_FILE;
-        private const char TESTS_FILE_DELIM_CORECT = ':';
-        private const string TESTS_FILE_DELIM_QUESTION = "---";
-        private const string TESTS_FILE_DELIM_TESTS = "===";
+        //private const char TESTS_FILE_DELIM_CORECT = ':';
+        //private const string TESTS_FILE_DELIM_QUESTION = "---";
+        //private const string TESTS_FILE_DELIM_TESTS = "===";
+		private const string TESTS_DIR;
 
         public ManageTests()
         {
             Stream streamTextFile;
 
-            string DataFileName = ConfigurationManager.AppSettings["TestsFileName"];
+            //string DataFileName = ConfigurationManager.AppSettings["TestsFileName"];
             string SolutionFileLocation = System.IO.Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName;
-            TESTS_DATA_FILE = SolutionFileLocation + "\\" + "DATA" + "\\" + DataFileName;
+            //TESTS_DATA_FILE = SolutionFileLocation + "\\" + "DATA" + "\\" + DataFileName;
+			TESTS_DIR = SolutionFileLocation + "\\" + "DATA" + "TESTS" + "\\";
 
             streamTextFile = File.Open(TESTS_DATA_FILE, FileMode.OpenOrCreate);
             streamTextFile.Close();
@@ -28,84 +30,29 @@ namespace DataAdministration
 
         public List<Test> GetTests()
         {
-            string line;
-            StreamReader streamReader;
+            //string line;
+            //StreamReader streamReader;
+
+            //streamReader = new StreamReader(TESTS_DATA_FILE, true);
 
             List<Test> TestsData = new List<Test>();
 
-            streamReader = new StreamReader(TESTS_DATA_FILE, true);
+			string[] TestsFoundFiles = Directory.GetFiles(TESTS_DIR, "*.txt");
+			
+			foreach(string f in TestsFoundFiles)
+			{
+				List<string> Test_data = streamReader.ReadAllLines(f).ToList();
+				TestsData.Add(Test(Test_data, f));
+			}
 
-            while (true)
-            {
-                //Test Name
-                //---
-                //Question
-                //0:answer1
-                //0:answer2
-                //1:answer3
-                //=== //end test
-                Test test = new Test();
+            //streamReader.Close();
 
-                line = streamReader.ReadLine();
+			if(TestsData.Length == 0)
+			{
+				return null;
+			}
 
-                if (line != String.Empty)
-                {
-                    test.Name = line;
-                }
-
-                line = streamReader.ReadLine();
-
-                Question question = new Question();
-
-                while (line != TESTS_FILE_DELIM_TESTS)
-                {
-                    while (line != TESTS_FILE_DELIM_QUESTION)
-                    {
-
-                    }
-
-                }
-                break;
-
-
-
-                userLoginData = line.Trim().Split(LOGIN_DATA_FILE_DELIM);
-                if (userLoginData[1] == username && userLoginData[2] == password)
-                    break;
-                userLoginData = Array.Empty<string>();
-            }
-            streamReader.Close();
-
-            if (userLoginData.Length == 0) return null;
-
-            // Get user data from another file
-            streamReader = new StreamReader(USER_DATA_FILE, true);
-            while ((line = streamReader.ReadLine()) != String.Empty)
-            {
-                //ID:EMAIL:FIRST_NAME:SECOND_NAME:BIRTH_DATE:OCUPATION
-                string[] userData = line.Trim().Split(LOGIN_DATA_FILE_DELIM);
-
-                //todo TryParse
-                //add test for userData if not string throw error or something
-                if (Convert.ToInt32(userData[0]) == Convert.ToInt32(userLoginData[0]))
-                {
-                    //Add a enum for these users
-                    switch (Convert.ToInt32(userLoginData[3]))
-                    {
-                        case 1:
-                            return new Admin(userData);
-                        case 2:
-                            return new Prof(userData);
-                        case 3:
-                            return new Stud(userData);
-                        default:
-                            return null;
-                    }
-                }
-            }
-
-            streamReader.Close();
-            return null;
+			return TestsData;
         }
     }
 }
--- ObjectDefines/Test.cs
+++ ObjectDefines/Test.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace ObjectDefines
 {
+	private const char TESTS_FILE_DELIM_CORECT = ':';
+	private const string TESTS_FILE_DELIM_QUESTION = "---";
+	private const string TESTS_FILE_DELIM_TESTS = "===";
+
     public class Test
     {
-        public String Name;
-        public Question[] Questions;
+		public String TestFileName { get; set; }
+        public String Name { get; set; }
+        public Question[] Questions { get; set; }
 
         public Test()
         {
@@ -21,5 +26,54 @@ namespace ObjectDefines
         {
             Questions = questions;
         }
+
+		public Test(string[] TestData, string TestFileName_t)
+		{
+			TestFileName = TestFileName_t;
+
+			//TestName
+			//question1
+			//1:answer1
+			//0:answer2
+			//0:answer4
+			//1:answer3
+			//---
+			//question2
+			//1:answer1
+			//0:answer2
+			//0:answer4
+			//1:answer3
+			//---
+
+			
+			int i = 0;
+
+			Name = TestData[i++];
+
+			Question question_temp = new Question();
+
+			while(true)
+			{
+				question_temp.name = TestData[i++];	
+
+				while (TestData[i] != TESTS_FILE_DELIM_QUESTION)		
+				{
+					string[] answer = TestData[i++].Trim().Split(TESTS_FILE_DELIM_CORECT);
+					int correct_num = Convert.ToInt32(answer[0]);
+					
+					Answer tempAnswer = new Answer(answer[1], (bool)correct_num);
+
+					question_temp.answers.Add(tempAnswer);
+				}
+
+				Questions.Add(question_temp);
+
+				if(i>=TestData.Length){
+					break;
+				}
+
+				question_temp = new Question();
+			}
+		}
     }
 }
